465a37f66db97cb88db21cbaa4e1a100d63bfba0
GingerBeardx <ecryptographer@gmail.com>
2018-07-10 20:29:36 -0600

    moved definitions to another file

--- server.py
+++ server.py
@@ -1,42 +1,12 @@
 from flask import Flask, render_template, session, redirect, request
+from utils import *
 import random, datetime
+
 app = Flask(__name__)
 app.key = random.randrange(0,15678895)
 app.secret_key = str(app.key)
 app.count = 0
 
-def init_vals():
-    try:
-        session['total_gold']
-        session['activities']
-    except KeyError:
-        session['total_gold'] = 0
-        session['activities'] = []
-
-def update_strings(location, add_gold):
-    if add_gold >= 0:
-        netted = "Earned"
-        effect = "green"
-    else:
-        netted = "Ouch! Played the odds and lost"
-        effect = "red"
-    time = datetime.datetime.now().strftime("%Y-%m-%d[-]%H:%M:%S")
-    string = "{} {} gold from the {}! ({})".format(netted, abs(add_gold), location, time)
-    session['activities'].append([effect, string])
-    app.count += 1
-        
-
-def gold_update(location):
-    if location == 'farm':
-        add_gold = random.randrange(10, 21)
-    elif location == 'cave':
-        add_gold = random.randrange(5, 11)
-    elif location == 'house':
-        add_gold = random.randrange(2, 6)
-    elif location == 'casino':
-        add_gold = random.randrange(-50, 51)
-    update_strings(location, add_gold)
-    session['total_gold'] += add_gold
 
 @app.route('/')
 def index():
---
+++ utils.py
@@ -0,0 +1,32 @@
+def init_vals():
+    try:
+        session['total_gold']
+        session['activities']
+    except KeyError:
+        session['total_gold'] = 0
+        session['activities'] = []
+
+def update_strings(location, add_gold):
+    if add_gold >= 0:
+        netted = "Earned"
+        effect = "green"
+    else:
+        netted = "Ouch! Played the odds and lost"
+        effect = "red"
+    time = datetime.datetime.now().strftime("%Y-%m-%d[-]%H:%M:%S")
+    string = "{} {} gold from the {}! ({})".format(netted, abs(add_gold), location, time)
+    session['activities'].append([effect, string])
+    app.count += 1
+        
+
+def gold_update(location):
+    if location == 'farm':
+        add_gold = random.randrange(10, 21)
+    elif location == 'cave':
+        add_gold = random.randrange(5, 11)
+    elif location == 'house':
+        add_gold = random.randrange(2, 6)
+    elif location == 'casino':
+        add_gold = random.randrange(-50, 51)
+    update_strings(location, add_gold)
+    session['total_gold'] += add_gold
